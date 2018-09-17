ff3a8b075d5a0d0f0ccb6a5002869ec718112ac8
Leonardo <leonardo.avila98@outlook.com>
2018-09-16 21:42:19 -0300

    Adicionado o banco, controller de login com view de cadastro e login

---
+++ whos-bad/whos-bad/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace whos_bad.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return RedirectToAction("Autenticar");
+        }
+
+        
+        public ActionResult Autenticar(string login, string senha)
+        {
+            return View();
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
