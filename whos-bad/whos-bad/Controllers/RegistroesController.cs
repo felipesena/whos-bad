57ad2c1af51f36d2c93e54e9eb21b1a3d758e3c3
Felipe Sena <felipedepaulasena@hotmail.com>
2018-09-18 10:14:15 -0300

    Adição de controllers

---
+++ whos-bad/whos-bad/Controllers/RegistroesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using whos_bad.Models;
+
+namespace whos_bad.Controllers
+{
+    public class RegistroesController : Controller
+    {
+        private whosBadDBEntities db = new whosBadDBEntities();
+
+        // GET: Registroes
+        public ActionResult Index()
+        {
+            var registro = db.Registro.Include(r => r.Usuario);
+            return View(registro.ToList());
+        }
+
+        // GET: Registroes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registro registro = db.Registro.Find(id);
+            if (registro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registro);
+        }
+
+        // GET: Registroes/Create
+        public ActionResult Create()
+        {
+            ViewBag.FKUserId = new SelectList(db.Usuario, "UserId", "Nome");
+            return View();
+        }
+
+        // POST: Registroes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RegistroId,Contexto,Atitude,Sentimento,Humor,Data,Pensamento,FKUserId")] Registro registro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Registro.Add(registro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FKUserId = new SelectList(db.Usuario, "UserId", "Nome", registro.FKUserId);
+            return View(registro);
+        }
+
+        // GET: Registroes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registro registro = db.Registro.Find(id);
+            if (registro == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FKUserId = new SelectList(db.Usuario, "UserId", "Nome", registro.FKUserId);
+            return View(registro);
+        }
+
+        // POST: Registroes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RegistroId,Contexto,Atitude,Sentimento,Humor,Data,Pensamento,FKUserId")] Registro registro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FKUserId = new SelectList(db.Usuario, "UserId", "Nome", registro.FKUserId);
+            return View(registro);
+        }
+
+        // GET: Registroes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registro registro = db.Registro.Find(id);
+            if (registro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registro);
+        }
+
+        // POST: Registroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Registro registro = db.Registro.Find(id);
+            db.Registro.Remove(registro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
